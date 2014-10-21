bbf2b62c9e2a0ff92c2e95e62b1ed766906a26ad
Olle Kvarnström <olle+git@iix.se>
2014-10-21 20:38:57 +0200

    split graveyard.load into graveyard file

--- ath_cliath/graveyard.py
+++ ath_cliath/graveyard.py
@@ -2,6 +2,8 @@ import shelve
 GRAVEYARD_FILEPATH = "save/.graveyard"
 
 def append(player):
+    # Append player to graveyard
+
     data = (str(player.name)
            ,str(player.fighter.level)
            ,str(player.dlevel)
@@ -9,5 +11,13 @@ def append(player):
            )
 
     file = shelve.open(GRAVEYARD_FILEPATH, 'w')
-    file['characters'].append(data)
+    file["characters"].append(data)
     file.close()
+
+def load():
+    # Load players from graveyard
+
+    file = shelve.open(GRAVEYARD_FILEPATH)
+    data = file["characters"]
+    file.close()
+    return data
