dcb64e8e101e86f2a1349d9c26e447b8cd2b7665
Olle Kvarnström <olle+git@iix.se>
2016-07-26 14:07:36 +0200

    Remove unrelated data

--- old/ath_cliath/Tile.py
+++
@@ -1,6 +0,0 @@
-class Tile:
-    def __init__(self, blocked, block_sight = None):
-        self.blocked = blocked
-        self.explored = False
-        self.in_fov = False
-        self.block_sight = blocked if block_sight is None else block_sight
